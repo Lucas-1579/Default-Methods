13ab949eed51bf0a216dedbde33f17ab6af9d4eb
Lucas Dias de Almeida <lucasdias1589@gmail.com>
2021-01-26 17:20:22 -0300

    Including Exception

--- src/services/InterestService.java
+++ src/services/InterestService.java
@@ -1,12 +1,14 @@
 package services;
 
+import java.security.InvalidParameterException;
+
 public interface InterestService {
 	
 	Double getInterestRate();
 	
 	default double payment(double amount, int months) {
 		if(months < 1) {
-			throw new IllegalStateException("");
+			throw new InvalidParameterException("Months must be greater than zero.");
 		}
 		else {
 			return amount * Math.pow(1.0 + getInterestRate() / 100, months);
